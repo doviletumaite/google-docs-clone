f209b1bcc0624ac881214dc93dd61bce6c19f568
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-01-31 17:47:44 +0100

    add style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import TextEditor from "./TextEditor";
+import "./TextEditor.css"
 
 function App() {
   return (
--- src/TextEditor.jsx
+++ src/TextEditor.jsx
@@ -2,6 +2,25 @@ import Quill from "quill"
 import "quill/dist/quill.snow.css"
 import { useCallback, useEffect, useRef } from "react"
 
+const TOOLBAR_OPTIONS = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+  
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+  
+    ['clean']     
+]
+
 export default function TextEditor() {
     
    const wrapperRef = useCallback(wrapper=> {
@@ -9,10 +28,10 @@ export default function TextEditor() {
         wrapper.innerHTML = ""
         const editor = document.createElement("div")
         wrapper.append(editor)
-        new Quill(editor, {theme: "snow"})
+        new Quill(editor, {theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS}})
     },[])
   return(
-      <div id="container" ref={wrapperRef}>
+      <div className="container" ref={wrapperRef}>
           text editor
       </div>
   )
