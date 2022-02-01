10772127fdb6cd3cad249e85270cb9edb276cdcd
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-02-01 19:15:02 +0100

    connected with socket

--- src/TextEditor.jsx
+++ src/TextEditor.jsx
@@ -1,6 +1,7 @@
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
-import { useCallback, useEffect, useRef } from "react"
+import { useCallback, useEffect, useState } from "react"
+import { io } from "socket.io-client"
 
 const TOOLBAR_OPTIONS = [
     ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
@@ -22,14 +23,53 @@ const TOOLBAR_OPTIONS = [
 ]
 
 export default function TextEditor() {
-    
+     const [socket, setSocket] = useState()
+     const [quill, setQuill] = useState()
    const wrapperRef = useCallback(wrapper=> {
        if (wrapper == null) return
         wrapper.innerHTML = ""
         const editor = document.createElement("div")
         wrapper.append(editor)
-        new Quill(editor, {theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS}})
+      const q =  new Quill(editor, {theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS}})
+      setQuill(q)
     },[])
+
+   
+    useEffect(() => {
+      setSocket(io("http://localhost:3001")) 
+      return () => {
+          socket.disconnect()
+      }
+    }, [])
+
+    useEffect(() => {
+        if (socket == null || quill == null) return
+      const handler = (delta, oldDelta, source) => {
+          if (source !== "user") return
+          socket.emit("send-changes", delta)
+      }
+      quill.on("text-change", handler)
+      return () => {
+          quill.off("text-change",handler )
+      }
+    }, [socket, quill])
+    useEffect(() => {
+        setSocket(io("http://localhost:3001")) 
+        return () => {
+            socket.disconnect()
+        }
+      }, [])
+  
+      useEffect(() => {
+          if (socket == null || quill == null) return
+        const handler = (delta) => {
+           quill.updateContents(delta)
+        }
+       socket.on("receive-changes", handler)
+        return () => {
+            socket.off("receive-changes",handler )
+        }
+      }, [socket, quill])
   return(
       <div className="container" ref={wrapperRef}>
           text editor
