e07dacde68c4ae7ef970d332685bc9dc2bb420b5
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-02-02 17:43:29 +0100

    up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import TextEditor from "./TextEditor";
 import "./TextEditor.css"
-
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
     <div>
-   <TextEditor/>
+      <Router>
+      <Switch> 
+      <Route path="/" exact> 
+      <Redirect to={`/documents/${uuidV4()}`}/>
+      </Route>
+      <Route path="/documents/:id" exact> 
+      </Route>
+       <TextEditor/>
+      </Switch>
+     </Router>
     </div>
   );
 }
